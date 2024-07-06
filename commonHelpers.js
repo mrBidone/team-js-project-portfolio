c5a533a426fd0a5c3c0d95d07d2d388f0407a29c
mrBidone <79172596+mrBidone@users.noreply.github.com>
2024-07-06 21:57:17 +0000

    Deploying to gh-pages from @ mrBidone/team-js-project-portfolio@fcc15e1a583190b958dabd39a92ec8ae79275cd1 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,14 @@
-import"./assets/vendor-e1b8bae9.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const i={reviewsList:document.querySelector(".reviews-list"),previousBtn:document.querySelector(".previous-btn"),nextBtn:document.querySelector(".next-btn")};i.previousBtn.addEventListener("click",r=>r.preventDefault());i.nextBtn.addEventListener("click",r=>r.preventDefault());
+import{a as c}from"./assets/vendor-8635c165.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const a={reviewsList:document.querySelector(".reviews-list"),previousBtn:document.querySelector(".previous-btn"),nextBtn:document.querySelector(".next-btn")};document.querySelector(".reviews-list");window.addEventListener("DOMContentLoaded",async()=>{try{(await c.get("https://portfolio-js.b.goit.study/api/reviews")).data.forEach(o=>{const r=document.createElement("div");r.classList.add("review-item"),r.innerHTML=`<li class="reviews-item">
+         <img
+           src="${o.avatar_url}"
+           alt="author's photo"
+           class="review-photo"
+           width="48px"
+           height="48px"
+         />
+         <p class="review-author">${o.author}</p>
+         <p class="review-text">
+           ${o.review}
+         </p>
+       </li>`,a.reviewsList.appendChild(r)})}catch(i){console.error("Error fetching reviews:",i)}});
 //# sourceMappingURL=commonHelpers.js.map
