d8a9b11faee203260ab79bf44819e18516c589c5
mrBidone <ab.bz@ukr.net>
2024-07-09 01:14:57 +0200

    fixed footer files

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './js/faq';
 import './js/projects';
 import './js/reviews';
 import './js/footer';
+import './js/covers';
 
 export default {
   contactForm: document.querySelector('.footer-form'),
@@ -15,30 +16,3 @@ export default {
   modalTitleEl: document.querySelector('.modal-title'),
   modalMessageEl: document.querySelector('.modal-message'),
 };
-
-
-const refs = {
-  itemCards: document.querySelectorAll('.marque-item'),
-  coverSection: document.querySelector('.covers-section'),
-};
-
-const observerConfig = {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0,
-};
-
-const animationObserver = new IntersectionObserver(
-  handleIntersection,
-  observerConfig
-);
-animationObserver.observe(refs.coverSection);
-
-function handleIntersection(entries) {
-  entries.forEach(entry => {
-    refs.itemCards.forEach(itemCard => {
-      itemCard.classList.toggle('animation', entry.isIntersecting);
-    });
-  });
-}
-
