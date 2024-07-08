0c42cb5af237037b44887408b6a99109b650c0d2
Nikita Boyarinov <niko.0707.niko@gmail.com>
2024-07-08 20:46:17 +0300

    footer js fix

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,24 +1,39 @@
-const contactForm = document.querySelector('.footer-form');
+import iziToast from "izitoast";
+import imageUrlError from '../img/footer/icon-error.svg';
+import refs from '../main.js';
 
-contactForm.addEventListener('submit', async function (e) {
+refs.contactForm.addEventListener('submit', async function (e) {
     e.preventDefault();
- 
-    const email = document.getElementById('user-email').value;
-    const comments = document.getElementById('user-comment').value;
-    if (!email || !comments) {
-        alert('Please fill in both fields');
+   
+    const email = refs.email.value;
+    const comments = refs.comments.value;
+    if (!email || !comments) { 
+        iziToast.error({
+            title: 'Error!',
+            titleSize: '16',
+            titleColor: 'var(--main-text-color)',
+            message: 'Please fill in both fields',
+            messageSize: '16',
+            messageColor: 'var(--main-text-color)',
+            backgroundColor: 'var(--accent-color)',
+            theme: 'dark',
+            position: 'center',
+            closeOnEscape: true,
+            closeOnClick: true,
+            iconUrl: imageUrlError,
+        });
         return;
     }
- 
+    
     localStorage.setItem('email', email);
     localStorage.setItem('comment', comments);
     const dataLoad = {
         email: email,
-        comment: comments
+        comment: comments,
     };
-
+    
     try {
-        const response = await fetch('https://portfolio-js.b.goit.study/api-docs', {
+        const response = await fetch('https://portfolio-js.b.goit.study/api/requests', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -28,41 +43,60 @@ contactForm.addEventListener('submit', async function (e) {
         const data = await response.json();
 
         if (data.error) {
-            console.log("Error: " + data.error);
+            iziToast.error({
+            title: 'Error!',
+            titleSize: '16',
+            titleColor: 'var(--main-text-color)',
+            message: `${data.error}`,
+            messageSize: '16',
+            messageColor: 'var(--main-text-color)',
+            backgroundColor: 'var(--accent-color)',
+            theme: 'dark',
+            position: 'bottomCenter',
+            closeOnEscape: true,
+            closeOnClick: true,
+            iconUrl: imageUrlError,
+            });
         } else {
-            contactForm.reset()
+            refs.contactForm.reset()
         }
-        modalTitleEl.textContent = data.title;
-        modalMessageEl.textContent = data.message;
+        
+        refs.modalTitleEl.textContent = data.title;
+        refs.modalMessageEl.textContent = data.message;
         openModalWindow();
     } catch (error) {
-        alert('Error: ' + error.message);
+        iziToast.error({
+            title: 'Error!',
+            titleSize: '16',
+            titleColor: 'var(--main-text-color)',
+            message: `${error.message}`,
+            messageSize: '16',
+            messageColor: 'var(--main-text-color)',
+            backgroundColor: 'var(--accent-color)',
+            theme: 'dark',
+            position: 'bottomCenter',
+            closeOnEscape: true,
+            closeOnClick: true,
+            iconUrl: imageUrlError,
+        });
     }
 });
 
 // <!-- Modal -->
-
-const backdropEl = document.querySelector('.footer-backdrop');
-const modalEl = document.querySelector('.modal')
-const closeModalBtnEl = document.querySelector('.close-btn');
-const modalTitleEl = document.querySelector('.modal-title');
-const modalMessageEl = document.querySelector('.modal-message');
-
 function openModalWindow() {
-    backdropEl.classList.add('is-open')
+    refs.backdropEl.classList.add('is-open')
 }
 function closeModalWindow() {
-    backdropEl.classList.remove('is-open')
+    refs.backdropEl.classList.remove('is-open')
 }
-
-closeModalBtnEl.addEventListener('click', closeModalWindow);
+refs.closeModalBtnEl.addEventListener('click', closeModalWindow);
 document.addEventListener('keydown', (e) => {
-    if (e.key === 'Escape' && backdropEl.classList.contains('is-open')) {
+    if (e.key === 'Escape' && refs.backdropEl.classList.contains('is-open')) {
         closeModalWindow();
     }
 })
 document.addEventListener('click', (e) => {
-    const click = e.composedPath().includes(modalEl);
+    const click = e.composedPath().includes(refs.modalEl);
     if (!click) {
         closeModalWindow();
     }
--- src/main.js
+++ src/main.js
@@ -4,3 +4,13 @@ import './js/faq';
 import './js/projects';
 import './js/reviews';
 import './js/footer';
+export default {
+  contactForm: document.querySelector('.footer-form'),
+  email: document.getElementById('user-email'),
+  comments: document.getElementById('user-comment'),
+  backdropEl: document.querySelector('.footer-backdrop'),
+  modalEl: document.querySelector('.modal'),
+  closeModalBtnEl: document.querySelector('.close-btn'),
+  modalTitleEl: document.querySelector('.modal-title'),
+  modalMessageEl: document.querySelector('.modal-message'),
+};
