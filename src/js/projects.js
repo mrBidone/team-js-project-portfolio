ff22023fcd10fc279bb6572224179a2d7a677909
mrBidone <ab.bz@ukr.net>
2024-07-09 23:31:13 +0200

    UPD project.js

--- src/js/projects.js
+++ src/js/projects.js
@@ -16,83 +16,30 @@ const swiperProject = new Swiper('#swiperProject', {
     enabled: true,
     onlyInViewport: true,
   },
-  mousewheel: false,
+  mousewheel: true,
   touch: true,
   centeredSlides: false,
+  loop: false,
   speed: 500,
   spaceBetween: 0,
-  slideToClickedSlide: true,
+  slideToClickedSlide: false,
   allowTouchMove: true,
-  slidesPerView: 1,
+
   width: 343,
+  autoplay: false,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
   breakpoints: {
     768: {
       slidesPerView: 1,
+
       width: 704,
     },
     1440: {
       slidesPerView: 1,
+
       width: 1376,
     },
-
-    mousewheel: true,
-    touch: true,
-    centeredSlides: false,
-    loop: false,
-    speed: 500,
-    spaceBetween: 0,
-    slideToClickedSlide: false,
-     allowTouchMove: true,
-
-    width: 343,
-    autoplay: false,
-    slidesPerView: 1 ,
-    slidesPerGroup:1,
-    breakpoints: {
-      768: {
-        slidesPerView: 1,
-
-        width: 704,
-      },
-      1440: {
-        slidesPerView: 1,
-
-        width: 1376,
-      },
-    },
-    on: {
-      slideChange: function() {
-        const nextBtn = document.querySelector('.swiper-proj-next');
-        const prevBtn = document.querySelector('.swiper-proj-prev');
-
-    if (this.isEnd) {
-          nextBtn.disabled = true;
-          nextBtn.classList.add('disabled');
-
-        } else {
-          nextBtn.disabled = false;
-          nextBtn.classList.remove('disabled');
-        }
-        if (this.isBeginning) {
-          prevBtn.disabled = true;
-          prevBtn.classList.add('disabled');
-
-        } else {
-          prevBtn.disabled = false;
-          prevBtn.classList.remove('disabled');
-        }
-
-
-     }
-    },
-  });
-
-
-
-
-
-
-
   },
   on: {
     slideChange: function () {
@@ -116,3 +63,45 @@ const swiperProject = new Swiper('#swiperProject', {
     },
   },
 });
+
+// mousewheel: false,
+// touch: true,
+// centeredSlides: false,
+// speed: 500,
+// spaceBetween: 0,
+// slideToClickedSlide: true,
+// allowTouchMove: true,
+// slidesPerView: 1,
+// width: 343,
+// breakpoints: {
+//   768: {
+//     slidesPerView: 1,
+//     width: 704,
+//   },
+//   1440: {
+//     slidesPerView: 1,
+//     width: 1376,
+//   },
+//   },
+//   on: {
+//     slideChange: function () {
+//       const nextBtn = document.querySelector('.swiper-proj-next');
+//       const prevBtn = document.querySelector('.swiper-proj-prev');
+
+//       if (this.isEnd) {
+//         nextBtn.disabled = true;
+//         nextBtn.classList.add('disabled');
+//       } else {
+//         nextBtn.disabled = false;
+//         nextBtn.classList.remove('disabled');
+//       }
+//       if (this.isBeginning) {
+//         prevBtn.disabled = true;
+//         prevBtn.classList.add('disabled');
+//       } else {
+//         prevBtn.disabled = false;
+//         prevBtn.classList.remove('disabled');
+//       }
+//     },
+//   },
+// });
