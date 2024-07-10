f46a80cd875d110806cfe2415e5094f4611a813d
KaterynaSokol <k.sokol2019@gmail.com>
2024-07-10 22:22:23 +0300

    add btn up

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -96,3 +96,24 @@ window.addEventListener('DOMContentLoaded', async () => {
     errorBlock.style.display = 'block';
   }
 });
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('myBtn').style.display = 'block';
+  } else {
+    document.getElementById('myBtn').style.display = 'none';
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('myBtn').addEventListener('click', topFunction);
+});
